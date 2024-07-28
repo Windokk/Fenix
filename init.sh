b4e44a470607c1bdbf83f8a7fa39214090bc1cd5
Windokk <68743762+Windokk@users.noreply.github.com>
2024-07-28 17:32:14 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #### WARNING #####
 # Do not execute this file ! #
-# It is a just a list of command you have to execute (one by one) to be able to use the i686-elf toolchain #
+# It is a just a list of command you have to execute to be able to use the x86_64-elf toolchain #
 
 
 export TARGET=x86_64-elf
