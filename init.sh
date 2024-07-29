6e014a6dbf0e76b84e63ac6d1fd85e2888cc507d
Windokk <68743762+Windokk@users.noreply.github.com>
2024-07-29 13:05:57 +0200

    Delete init.sh

--- init.sh
+++
@@ -1,8 +0,0 @@
-#### WARNING #####
-# Do not execute this file ! #
-# It is a just a list of command you have to execute to be able to use the x86_64-elf toolchain #
-
-
-export TARGET=x86_64-elf
-export PATH="$PREFIX/bin:$PATH"
-export PATH="$HOME/Downloads/opt/cross/bin:$PATH"
